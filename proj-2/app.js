069350aa3ab8834cda0ac7b4805f3de50941973b
João Oliveira <jmlcoliveira@gmail.com>
2022-11-14 18:00:09 +0000

    added option to change scale

--- proj-2/app.js
+++ proj-2/app.js
@@ -1,7 +1,7 @@
-import { buildProgramFromSources, loadShadersFromURLS, setupWebGL } from "../../libs/utils.js";
-import { ortho, lookAt, flatten, mult, mat4, vec4 } from "../../libs/MV.js";
+import { buildProgramFromSources, loadShadersFromURLS, setupWebGL } from "../libs/utils.js";
+import { ortho, lookAt, flatten, mult, mat4, vec4, inverse } from "../libs/MV.js";
 import { GUI } from "../libs/dat.gui.module.js";
-import {modelView, loadMatrix, multRotationY, multRotationX, multRotationZ, multTranslation, multScale, pushMatrix, popMatrix  } from "../../libs/stack.js";
+import {modelView, loadMatrix, multRotationY, multRotationX, multRotationZ, multTranslation, multScale, pushMatrix, popMatrix  } from "../libs/stack.js";
 
 import * as SPHERE from '../../libs/objects/sphere.js';
 import * as CUBE from '../../libs/objects/cube.js';
@@ -18,31 +18,33 @@ let speed = 1/60.0;     // Speed (how many days added to time on each render pas
 let mode;               // Drawing mode (gl.LINES or gl.TRIANGLES)
 let animation = true;   // Animation is running
 
-const HELICOPTER_SCALE = 10;    // scale that will apply to each planet and satellite
+const HELICOPTER_SCALE = 1;    // scale that will apply to each planet and satellite
 const ORBIT_SCALE = 1/60;   // scale that will apply to each orbit around the sun
 
-const HELICOPTER_LENGHT = 10 * HELICOPTER_SCALE;
+const HELICOPTER_LENGHT = 10;
 const TRAJECTORY_RADIUS = 30;
 
-const BLADE_LENGTH = 3 * HELICOPTER_SCALE;
-const BLADE_WIDTH = 0.4 * HELICOPTER_SCALE;
+const BLADE_LENGTH = 80;
+const BLADE_WIDTH = 5;
 
-const ROTOR_RADIUS = 0.25 * HELICOPTER_SCALE;
+const ROTOR_RADIUS = 0.08;
+const ROTOR_HEIGHT = 0.8;
 const ROTOR_SPEED = 60;
 
-const TAIL_ROTOR_SCALE = 0.01 * HELICOPTER_SCALE;
+const TAIL_ROTOR_SCALE = 0.01;
 
-const TAIL_LENGTH = 5 * HELICOPTER_SCALE;
+const TAIL_LENGTH = 5;
 
-const TAIL_TIP_LENGTH = 0.5 * HELICOPTER_SCALE;
+const TAIL_TIP_LENGTH = 0.5;
 
 const VP_DISTANCE = 10;
-var camera = { x:0, y:0, z:0};
+var camera = { x:0, y:0, z:0, scale:1};
 
 const gui = new GUI();
-gui.add(camera, "x", 0, 4*Math.PI, 0.1).name("X");
-
-
+gui.add(camera, "x", -10, 10, 0.1).name("X");
+gui.add(camera, "y", -10, 10, 0.1).name("Y");
+gui.add(camera, "z", -10, 10, 0.1).name("Z");
+gui.add(camera, "scale", 0, 5, 0.1).name("Scale");
 
 function setup(shaders)
 {
@@ -106,20 +108,45 @@ function setup(shaders)
     }
 
     function blade() {
-        multScale([BLADE_LENGTH, 1, BLADE_WIDTH]);
+        multScale([BLADE_LENGTH, 0.3, BLADE_WIDTH]);
         uploadModelView();
         SPHERE.draw(gl, program, mode);
     }
 
+    function rotor() {
+        pushMatrix();
+            multScale([ROTOR_RADIUS, ROTOR_HEIGHT, ROTOR_RADIUS]);
+            multTranslation([5, 1.6, 0]);
+            multRotationY(time*ROTOR_SPEED);
+            uploadModelView();
+            CYLINDER.draw(gl, program, mode);
+            pushMatrix();
+                multRotationY(360/3);
+                multTranslation([BLADE_LENGTH/2, 0.2, 0]);
+                blade();
+            popMatrix();
+            pushMatrix();
+                multRotationY(360*2/3);
+                multTranslation([BLADE_LENGTH/2, 0.2, 0]);
+                blade();
+            popMatrix();
+            pushMatrix();
+                multRotationY(360*3/3);
+                multTranslation([BLADE_LENGTH/2, 0.2, 0]);
+                blade();
+            popMatrix();
+        popMatrix();
+    }
+
     function tailRotor() {
         multScale([TAIL_ROTOR_SCALE, TAIL_ROTOR_SCALE, TAIL_ROTOR_SCALE]);
+        multRotationY(time*ROTOR_SPEED);
         pushMatrix();
             multScale([ROTOR_RADIUS, ROTOR_RADIUS, ROTOR_RADIUS]);
             uploadModelView();
             CYLINDER.draw(gl, program, mode);
         popMatrix();
         pushMatrix();
-            multRotationY(time*ROTOR_SPEED);
             pushMatrix();
                 multRotationY(360);
                 multTranslation([BLADE_LENGTH/2, 0, 0]);
@@ -131,33 +158,6 @@ function setup(shaders)
                 blade();
             popMatrix();
         popMatrix();
-
-    }
-    function rotor() {
-        pushMatrix();
-            multScale([ROTOR_RADIUS, ROTOR_RADIUS, ROTOR_RADIUS]);
-            uploadModelView();
-            CYLINDER.draw(gl, program, mode);
-        popMatrix();
-        pushMatrix();
-            multRotationY(time*ROTOR_SPEED);
-            pushMatrix();
-                multRotationY(360/3);
-                multTranslation([BLADE_LENGTH/2, 0, 0]);
-                blade();
-            popMatrix();
-            pushMatrix();
-                multRotationY(360*2/3);
-                multTranslation([BLADE_LENGTH/2, 0, 0]);
-                blade();
-            popMatrix();
-            pushMatrix();
-                multRotationY(360*3/3);
-                multTranslation([BLADE_LENGTH/2, 0, 0]);
-                blade();
-            popMatrix();
-        popMatrix();
-
     }
 
     function tailTip() {
@@ -174,27 +174,41 @@ function setup(shaders)
     }
 
     function tail() {
-        multScale(0, 2, 0)
+        multScale([1, 2, 1]);
+        multTranslation([3, 0, 0]);
         uploadModelView();
         SPHERE.draw(gl, program, mode);
+        tailTip();
     }
 
     function helicopterBody() {
-
-
+        pushMatrix();
+            multScale([5, 2, 2])
+            uploadModelView();
+            SPHERE.draw(gl, program, mode);
+            multScale([1/5, 1/2, 1/2])
+            pushMatrix();
+                tail();
+            popMatrix();
+            pushMatrix();
+                rotor();
+            popMatrix();
+            pushMatrix();
+                landingGear();
+            popMatrix();
+        popMatrix();
     }
 
     function helicopter()
     {
         // Don't forget to scale the sun, rotate it around the y axis at the correct speed
-        multScale([HELICOPTER_LENGHT/4, HELICOPTER_LENGHT/4, HELICOPTER_LENGHT/2]);
-        multRotationY(360*time/TRAJECTORY_RADIUS);
+        multScale([camera.scale, camera.scale, camera.scale]);
+        //multRotationY(360*time/TRAJECTORY_RADIUS);
 
         // Send the current modelview matrix to the vertex shader
         uploadModelView();
 
-        // Draw a sphere representing the sun
-        SPHERE.draw(gl, program, mode);
+        helicopterBody();
     }
 
     function render()
@@ -213,12 +227,18 @@ function setup(shaders)
             vec4(0, 1, 0, 0),
             vec4(0, 0, Math.sin(camera.x), 0),
             vec4(0, 0, 0, 1));
+        
     
-        loadMatrix(mult(transformMatrix ,lookAt([0, VP_DISTANCE/2, VP_DISTANCE], [0,0,0], [0,1,0])));
+        loadMatrix(lookAt([camera.x, camera.y, camera.z], [0, 0, 0]/*[camera.x, camera.y, camera.z]*/, [0,1,0]));
 
         uploadModelView();
-        CYLINDER.draw(gl, program, mode);
-        //tailTip();
+        
+        pushMatrix();
+            //floor();
+        popMatrix();
+        pushMatrix();
+            helicopter();
+        popMatrix();
     }
 }
 
